268c676854bda2f022ea19682d3748c9cb3e2fb8
Georgi Grozdanov <grozdanovgg@gmail.com>
2017-09-26 02:01:08 +0300

    Separated templates for footer dynamic content

--- src/public/js/controllers/home.js
+++ src/public/js/controllers/home.js
@@ -3,9 +3,11 @@ import { templater } from '../utils/templater.js';
 
 const homeController = {
     get() {
-        templater.get('home')
-            .then(template => {
-                $('#contents').html(template());
+        Promise.all([templater.get('home'), templater.get('footer-recent'), templater.get('footer-archived')])
+            .then(templates => {
+                $('#contents').html(templates[0]());
+                $('#footer-recent').html(templates[1]());
+                $('#footer-archived').html(templates[2]());
                 // toastr.success("Welcome home!");
             });
     }
