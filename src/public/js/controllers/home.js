0691bced467aa4b5cd085ae8b4d443dedfcfa169
Georgi Grozdanov <grozdanovgg@gmail.com>
2017-09-26 02:33:50 +0300

    Home page started

--- src/public/js/controllers/home.js
+++ src/public/js/controllers/home.js
@@ -3,9 +3,9 @@ import { templater } from '../utils/templater.js';
 
 const homeController = {
     get() {
-        Promise.all([templater.get('home'), templater.get('footer-recent'), templater.get('footer-archived')])
+        Promise.all([templater.get('home'), templater.get('shared/footer-recent'), templater.get('shared/footer-archived')])
             .then(templates => {
-                $('#contents').html(templates[0]());
+                $('#main-root').html(templates[0]());
                 $('#footer-recent').html(templates[1]());
                 $('#footer-archived').html(templates[2]());
                 // toastr.success("Welcome home!");
