15a45e97c88427f19c2f8cfa766d8fbd66c407b3
Georgi Grozdanov <grozdanovgg@gmail.com>
2017-10-06 17:20:00 +0300

    Improved footer. Improved responsive design

--- src/public/js/controllers/home.js
+++ src/public/js/controllers/home.js
@@ -6,14 +6,16 @@ const homeController = {
         Promise.all([
             templater.get('home/home'),
             templater.get('home/home-carousel'),
+            templater.get('home/home-bubble'),
             templater.get('shared/footer-recent'),
             templater.get('shared/footer-archived')
         ])
             .then(templates => {
                 $('#main-root').html(templates[0]());
                 $('#carousel-container').html(templates[1]());
-                $('#footer-recent').html(templates[2]());
-                $('#footer-archived').html(templates[3]());
+                $('#secondary-root').html(templates[2]());
+                $('#footer-recent').html(templates[3]());
+                $('#footer-archived').html(templates[4]());
                 // toastr.success("Welcome home!");
             })
             .then(() => {
