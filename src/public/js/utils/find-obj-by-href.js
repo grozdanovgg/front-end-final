ca16a62b0b77b5021c6c0dcb1fa7c800cf9486ae
Georgi Grozdanov <grozdanovgg@gmail.com>
2017-10-11 10:18:32 +0300

    temporary remove files

--- src/public/js/utils/find-obj-by-href.js
+++
@@ -1,9 +0,0 @@
-export function findObjByHref(href, myArray) {
-    console.log(href);
-    console.log(myArray);
-    for (var i = 0; i < myArray.length; i++) {
-        if (myArray[i].href === href) {
-            return myArray[i];
-        }
-    }
-}
--- src/public/js/utils/helper-string-trim.js
+++
@@ -1,9 +0,0 @@
-const stringTrim = {
-    do() {
-        Handlebars.registerHelper("stringTrim", function (string, length) {
-            return string.slice(0, length);
-        });
-    }
-};
-
-export { stringTrim };
